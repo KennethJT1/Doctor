7fda22b1358acf71af37950e7198e02b52cebf87
Kenneth <deca@DECAGONs-MacBook-Air.local>
2024-08-05 08:36:04 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import { connectedDB } from "./config";
+import cors from "cors";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use("/", userRouter);
 app.use("/doctor", doctorRouter);
